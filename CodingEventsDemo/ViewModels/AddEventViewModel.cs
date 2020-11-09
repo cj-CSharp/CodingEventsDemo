a8180c4203d139d73dfff8d2098bb728a2435efc
cjconnor18 <cjallen1988@gmail.com>
2020-11-09 13:00:04 -0600

    videos complete chapter 15

---
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage ="Name needs to be between 3 and 50 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(500, ErrorMessage = "Description has a maximum length of 500 characters.")]
+        public string Description { get; set; }
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
+    }
+}
