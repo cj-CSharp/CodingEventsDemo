55543233a9184eb8a507018c547352aa8205e67b
cjallen1988 <cjallen1988@gmail.com>
2020-12-02 21:34:20 -0600

    studio complete

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEventsDemo.Controllers
@@ -21,6 +23,29 @@ namespace CodingEventsDemo.Controllers
             ViewBag.categories = context.Categories.ToList();
 
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpPost("Create")]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                EventCategory newCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                };
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/EventCategory");
+            }
+            return View("Create", addEventCategoryViewModel);
         }
     }
 }
