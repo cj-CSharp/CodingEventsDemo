cbe3cbcf7cee500315e80883d4c40666b5f4d0c4
cjallen1988 <cjallen1988@gmail.com>
2020-12-06 11:34:18 -0600

    One to many relationship complete

--- CodingEventsDemo/Migrations/20200706171758_InitialMigration.cs
+++ CodingEventsDemo/Migrations/20200706171758_InitialMigration.cs
@@ -7,27 +7,27 @@ namespace CodingEventsDemo.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Events",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    Name = table.Column<string>(nullable: true),
-                    Description = table.Column<string>(nullable: true),
-                    ContactEmail = table.Column<string>(nullable: true),
-                    Type = table.Column<int>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Events", x => x.Id);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "Events",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<int>(nullable: false)
+            //            .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+            //        Name = table.Column<string>(nullable: true),
+            //        Description = table.Column<string>(nullable: true),
+            //        ContactEmail = table.Column<string>(nullable: true),
+            //        Type = table.Column<int>(nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Events", x => x.Id);
+            //    });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Events");
+            //migrationBuilder.DropTable(
+            //    name: "Events");
         }
     }
 }
